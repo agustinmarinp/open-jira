72b48ca7d265926cb43901dcaed9eed0561721b2
Agustin Marin Posadas <agustinmarindj@hotmail.com>
2023-01-30 21:03:18 +0100

    first installations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+
+const basicTheme = createTheme();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
